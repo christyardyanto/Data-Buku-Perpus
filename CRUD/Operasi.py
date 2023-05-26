ebe84eb923050b29088953c19c50a63b9e56dca5
christyardyanto <133240615+christyardyanto@users.noreply.github.com>
2023-05-27 01:48:56 +0700

    Add files via upload

---
+++ CRUD/Operasi.py
@@ -0,0 +1,121 @@
+from time import time
+from . import Database
+from .Util import random_string
+import time
+# import os
+import shutil
+
+
+def delete(no_buku):
+    try:
+        with open(Database.DB_NAME,'r') as file:
+            counter = 0
+            while(True):
+                content = file.readline()
+                if len(content) == 0:
+                    with open("data_temp.txt",'w',encoding="utf-8") as temp_file:
+                        temp_file.write(content)
+                elif counter == no_buku - 1:
+                    pass
+                counter += 1
+                                                            
+                # if len(content) == 0:
+                #     break
+                # elif counter == no_buku - 1:
+                #     pass
+                # else:
+                #     with open("data_temp.txt",'w',encoding="utf-8") as temp_file:
+                #         temp_file.write(content)
+                # counter += 1
+    except:
+        print("database error")
+
+    shutil.copyfile('data_temp.txt',Database.DB_NAME) 
+
+    # os.rename("data_temp.txt",Database.DB_NAME)
+
+
+
+def update(no_buku,pk,data_add,tahun,judul,penulis):
+    data = Database.TEMPLATE.copy()
+
+    data["pk"] = pk
+    data["date_add"] = data_add
+    data["penulis"] = penulis + Database.TEMPLATE["penulis"][len(penulis):]
+    data["judul"] = judul + Database.TEMPLATE["judul"][len(judul):]
+    data["tahun"] = str(tahun)
+    data_str = f'\n{data["pk"]},{data["date_add"]},{data["penulis"]},{data["judul"]},{data["tahun"]}'
+    
+    panjang_data = len(data_str)
+
+    try:
+        with(open(Database.DB_NAME,'r+',encoding ="utf-8")) as file:
+            file.seek(panjang_data*(no_buku-1))
+            file.write(data_str)
+    except:
+        print("Error dalam update data")
+
+
+
+
+def create(tahun,judul,penulis):
+    data = Database.TEMPLATE.copy()
+
+    data["pk"] = random_string(6)
+    data["date_add"] = time.strftime("%Y-%m-%d-%H-%M-%S%z",time.gmtime())
+    data["penulis"] = penulis + Database.TEMPLATE["penulis"][len(penulis):]
+    data["judul"] = judul + Database.TEMPLATE["judul"][len(judul):]
+    data["tahun"] = str(tahun)
+    data_str = f'{data["pk"]},{data["date_add"]},{data["penulis"]},{data["judul"]},{data["tahun"]}\n'
+   
+    try:
+        with open(Database.DB_NAME,'a',encoding="utf-8") as file:
+            file.write(data_str)
+    except:
+        print("Data salah, Gagal, Silahkan Ulangi")    
+
+def create_first_data():
+    penulis = input("Penulis: ")
+    judul = input("Judul: ")
+    while(True):
+        try:
+            tahun = int(input("Tahun\t: "))
+            if len(str(tahun)) == 4:
+                break
+            else:       
+                print("Tahun harus angka dan 4 karakter, Silahkan masukkan tahun kembali (yyyy)")    
+        except:
+            print("Tahun harus angka, Silahkan masukkan tahun kembali (yyyy)")
+
+    data = Database.TEMPLATE.copy()
+
+    data["pk"] = random_string(6)
+    data["date_add"] = time.strftime("%Y-%m-%d-%H-%M-%S%z",time.gmtime())
+    data["penulis"] = penulis + Database.TEMPLATE["penulis"][len(penulis):]
+    data["judul"] = judul + Database.TEMPLATE["judul"][len(judul):]
+    data["tahun"] = str(tahun)
+    data_str = f'{data["pk"]},{data["date_add"]},{data["penulis"]},{data["judul"]},{data["tahun"]}\n'
+    print(data_str)
+    
+    try:
+        with open(Database.DB_NAME,'w',encoding="utf-8") as file:
+            file.write(data_str)
+    except:
+        print("Gagal, Silahkan Ulangi")
+
+def read(**kwargs):
+    try:
+        with open(Database.DB_NAME, 'r') as file:
+            content = file.readlines()
+            jumlah_buku = len(content)        
+            if "index" in kwargs:
+                index_buku = kwargs["index"]-1 
+                if index_buku < 0 or index_buku > jumlah_buku:
+                    return False
+                else:
+                    return content[index_buku]
+            else:
+                return content
+    except:
+        print("Membaca database error")
+        return False
